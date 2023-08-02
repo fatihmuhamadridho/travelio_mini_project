ab39eb237e1cb7226782b0fd74db12b61d2299ac
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-08-03 02:11:59 +0700

    feat: 🎸 init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles/App.scss";
+import "bulma";
+
+export default function App() {
+  return (
+    <div>
+      <button className="button is-primary">Button</button>
+    </div>
+  );
+}
