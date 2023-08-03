af3d8019500bdc6b772a66574b9f828bf0a2f4a1
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-08-03 12:26:08 +0700

    feat: 🎸 setup backend

---
+++ backend/index.ts
@@ -0,0 +1,27 @@
+import { default as express } from 'express';
+import { default as bodyParser } from 'body-parser';
+import { default as cors } from 'cors';
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+const corsOptions = { origin: '*', optionsSuccessStatus: 200 };
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', (req: any, res: any) => {
+  res.status(500).json({
+    code: res.statusCode,
+    status: false,
+    message: 'Your endpoint is incorrect, please recheck abaout your endpoint..',
+    env: process.env.NODE_ENV
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is successfully running on port http://localhost:${PORT}`);
+});
+
+export default app;
